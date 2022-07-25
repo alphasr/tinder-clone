525a0d25a7d39805324e1ddadfdcbbff1d53a4db
alphasr <nilaybaranwal@gmail.com>
2022-07-26 01:51:00 +0200

    added second card

--- App.js
+++ App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import {View, StyleSheet, Pressable, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, useWindowDimensions} from 'react-native';
 import Card from './src/components/TinderCard';
 import users from './assets/data/users';
 import Animated, {
+  interpolate,
   useAnimatedGestureHandler,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
 } from 'react-native-reanimated';
 import {
   PanGestureHandler,
   GestureHandlerRootView,
 } from 'react-native-gesture-handler';
-const App = () => {
-  const translateX = useSharedValue(0);
 
+const ROTATION = 60;
+
+const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
+
+  const currentProfile = users[currentIndex];
+  const nextProfile = users[nextIndex];
+
+  const {width: screenWidth} = useWindowDimensions();
+  const hiddenTranslateX = 2 * screenWidth;
+  const translateX = useSharedValue(0);
+  const rotate = useDerivedValue(
+    () =>
+      interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) +
+      'deg',
+  );
   const cardStyle = useAnimatedStyle(() => ({
     transform: [
       {
         translateX: translateX.value,
       },
+      {
+        rotate: rotate.value,
+      },
     ],
+  }));
+
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(
+          translateX.value,
+          [-hiddenTranslateX, 0, hiddenTranslateX],
+          [1, 0.8, 1],
+        ),
+      },
+    ],
+    opacity: interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.5, 1],
+    ),
   }));
   const gestureHandler = useAnimatedGestureHandler({
     onStart: (_, context) => {
@@ -30,14 +67,20 @@ const App = () => {
     },
     onEnd: () => {
       console.warn('ayla');
+      setCurrentIndex(1);
     },
   });
   return (
     <View style={styles.pageContainer}>
-      <GestureHandlerRootView style={styles.pageContainer}>
+      <GestureHandlerRootView style={styles.gestContainer}>
+        <View style={styles.nextCardContainer}>
+          <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+            <Card user={nextProfile} />
+          </Animated.View>
+        </View>
         <PanGestureHandler onGestureEvent={gestureHandler}>
           <Animated.View style={[styles.animatedCard, cardStyle]}>
-            <Card user={users[2]} />
+            <Card user={currentProfile} />
           </Animated.View>
         </PanGestureHandler>
       </GestureHandlerRootView>
@@ -51,8 +94,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
   },
+  gestContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    width: '100%',
+  },
   animatedCard: {
     width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  nextCardContainer: {
+    ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
     alignItems: 'center',
   },
